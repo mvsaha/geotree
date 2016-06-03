c286a2cb2269210fa5c3e139590da680feb2656f
mvs <mvs>
2016-06-03 02:22:45 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+config = {
+    'description': 'geotree',
+    'author': 'mvsaha',
+    'url': 'https://github.com/mvsaha/geotree',
+    'version': 1.0,
+    'name': 'geotree'
+}
+
+setup(**config)
